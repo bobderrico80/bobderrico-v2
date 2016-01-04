6f94bcc424005af38495309baf97ad953b725491
Bob D'Errico <bob@bobderrico.com>
2016-01-03 21:45:20 -0500

    Removes references to bootstrap

--- themes/bobderrico/header.php
+++ themes/bobderrico/header.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package bobderrico
+ * @package _s
  */
 
 ?><!DOCTYPE html>
@@ -21,31 +21,33 @@
   </head>
 
   <body <?php body_class(); ?>>
-    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'bobderrico'); ?></a>
+    <div id="page" class="site">
+      <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', '_s'); ?></a>
 
-    <header role="banner">
-      <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?= get_bloginfo('name'); ?> - <?= get_bloginfo('description') ?></a>
-          </div>
+      <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+          <?php
+            if (is_front_page() && is_home()) : ?>
+              <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                        rel="home"><?php bloginfo('name'); ?></a></h1>
+            <?php else : ?>
+              <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                       rel="home"><?php bloginfo('name'); ?></a></p>
+              <?php
+            endif;
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <?php wp_nav_menu(['theme_location' => 'primary',
-                               'menu_id' => 'primary-menu',
-                               'container' => false,
-                               'menu_class' => 'nav navbar-nav']); ?>
-          </div>
-        </div>
-      </nav>
-    </header>
+            $description = get_bloginfo('description', 'display');
+            if ($description || is_customize_preview()) : ?>
+              <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+              <?php
+            endif; ?>
+        </div><!-- .site-branding -->
 
-    <div id="content" class="site-content">
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+          <button class="menu-toggle" aria-controls="primary-menu"
+                  aria-expanded="false"><?php esc_html_e('Primary Menu', '_s'); ?></button>
+          <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+        </nav><!-- #site-navigation -->
+      </header><!-- #masthead -->
+
+      <div id="content" class="site-content">
--- themes/bobderrico/inc/util.php
+++ themes/bobderrico/inc/util.php
@@ -4,13 +4,3 @@
    * @package bobderrico
    */
 
-  /*
-   * Adds .active class to current nav menu items
-   */
-  function special_nav_class($classes, $item) {
-    if (in_array('current-menu-item', $classes)) {
-      $classes[] = 'active ';
-    }
-    return $classes;
-  }
-  add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
